8d80304310a982da773d9f20e91790ee7f996f07
catline <volos37@yandex.ru>
2024-04-28 23:46:15 +0300

    without addition variable

--- exchange/main.cpp
+++ exchange/main.cpp
@@ -4,14 +4,13 @@ int main()
 {
     int a = 42;
     int b = 153;
-    int c;
     std::cout << "a: " << a << "\n";
     std::cout << "b: " << b << "\n";
 
     std::cout << "swap values...\n";
-    c = a;
-    a = b;
-    b = c;
+    b += a;
+    a = b - a;
+    b = b - a;
 
     std::cout << "a: " << a << "\n";
     std::cout << "b: " << b << "\n";
